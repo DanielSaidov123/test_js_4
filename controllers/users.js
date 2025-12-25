878614a22ec8c13d05b6f926eb381ff2af6efebf
DanielSaidov123 <ddny3622@gmail.com>
2025-12-25 11:24:47 +0200

    finish

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-import { readUsers, writeUsers } from "../utils/index.js";
+import { readReceipts, readUsers, writeUsers } from "../utils/index.js";
 
 export const CreateUser = async (req, res) => {
   try {
@@ -21,6 +21,49 @@ export const CreateUser = async (req, res) => {
     await writeUsers(userslist);
 
     res.status(201).send({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+
+export const UserPurchaseSummary = async (req, res) => {
+  try {
+    const receiptslist = await readReceipts();
+ 
+    
+    const user = receiptslist.find((r) => r.username === req.params.username);
+    
+    if (!user) {
+      return res.status(401).send({ err: "There is already such a user. " });
+    }
+    
+    const receiptsBYusername = receiptslist.filter(
+      (r) => r.username === req.params.username
+    );
+    if (receiptsBYusername.length === 0) {
+      const obj = {
+        totalTicketsBought: 0,
+        events: ["event name", "event name"],
+        averageTicketsPerEvent: 0,
+      };
+      return res.status(200).send(obj);
+    }
+    let sum = 0;
+    const arrnameevent=[]
+    for (let i = 0; i < receiptsBYusername.length; i++) {
+      sum += receiptsBYusername[i].icketsBought;
+      console.log(arrnameevent.includes(receiptsBYusername.eventName));
+      console.log(receiptsBYusername.eventName);
+      
+      if(!arrnameevent.includes(receiptsBYusername[i].eventName)) {
+        arrnameevent.push(receiptsBYusername[i].eventName)
+      }
+    }
+    for (let i = 0; i <arrnameevent.length ; i++) {
+
+     }
+    return res.status(200).send({totalTicketsBought:sum,events:arrnameevent,averageTicketsPerEvent:sum/arrnameevent.length});
+  
   } catch (err) {
     res.status(500).send({ err: err.message });
   }
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { CreateUser } from "../controllers/users.js"
+import { CreateUser, UserPurchaseSummary } from "../controllers/users.js"
 import { CreateTickets } from "../controllers/Tickets.js";
 
 const router = express.Router();
 
 router.route('/register').post(CreateUser)
 router.route('/tickets/buy').post( CreateTickets)
+router.route('/:username/summary').post( UserPurchaseSummary)
 
 
 
